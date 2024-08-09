634e0287d07fbe8e45fa3d0fe06f99ea01686001
Solution Hod <solutionhod@gmail.com>
2024-08-09 14:16:48 +0100

    ps

---
+++ ps.js
@@ -0,0 +1,81 @@
+function sumnumbers(a, b) {
+    //inputs
+    // a: a number
+    // b: b number
+    //output:
+    // a+b
+    return a + b;
+}
+
+function maxnumber(c, d) {
+    //inputs
+    //c: c number
+    //d: d number
+    //output:
+    //IF c > d THEN 
+    return c 
+    //ELSE 
+    return d
+    //END IF
+}
+
+function checkevenorodd(number) {
+    // Input the number
+
+   // IF (number MOD 2) == 0 THEN
+        return "even."
+    //ELSE
+        return "odd"
+    //ENDIF
+
+}
+    
+function celsiustofahrenheit(celcius) {
+    //input temperature
+    fahrenheit = (celcius * 9/5) + 32
+    return fahrenheit
+    
+}
+function checknumber(number) {
+    //input a number
+     
+    //IF number > 0 THEN 
+    return "positive"
+    //ELSE IF number < 0
+    return "negative"
+    //ELSE
+    return "zero"
+//END IF
+}
+
+function greetuser(name) {
+    
+}
+
+function minutestoseconds(minutes) {
+    //input minutes
+    seconds = minutes * 60
+    return seconds
+}
+
+function areaofrectangle(width, height) {
+    //input width
+    //input height
+    area = height * width
+    return area
+}
+
+function findlargest(num1, num2, num3) {
+    //input num1
+    //input num2
+    //input num3
+    //IF (num1 > num2) and (num1 > num3) THEN
+    return "num1"
+    //ELSE IF (num1 < num2) and (num2 > num3) 
+    return "num2"
+    //ELSE
+    return "num3"
+    //END IF
+        
+    }
+    
